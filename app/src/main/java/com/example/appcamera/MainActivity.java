60b29d93bf6de1207ca34801bdc9a1d58654ace0
victormerch <1Victormerch@gmail.com>
2021-10-25 16:19:29 +0200

    Adaptacion de imagen con layout de ImageView

--- app/src/main/java/com/example/appcamera/MainActivity.java
+++ app/src/main/java/com/example/appcamera/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.appcamera;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,11 +21,16 @@ import android.widget.ImageView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+
     private Button btnCamera;
     private ImageView iv;
     private String currentPhotoPath;
+    private File image = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,44 +42,107 @@ public class MainActivity extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abrirCamara();
+                try {
+                    abrirCamara();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
 
     }
 
-    private void abrirCamara(){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        }
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-        startActivityForResult(intent,1);
-
-
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
+    private void abrirCamara() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imgB = (Bitmap) extras.get("data");
-
-            iv.setImageBitmap(imgB);
-            File path = getApplicationContext().getFilesDir();
-            File foto = new File(path,"foto.jpg");
-
-            try {
-                FileOutputStream os = new FileOutputStream(foto);
-                imgB.compress(Bitmap.CompressFormat.JPEG,100,os);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
+        // Create the File where the photo should go
+        File photoFile = null;
+        photoFile = createImageFile();
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, 1);
         }
     }
 
+    protected File getFile(){
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photo = new File(path,"imatge.jpg");
+        return photo;
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri fileUri = null;
+
+        fileUri = Uri.fromFile(image);
+
+        iv.setImageURI(fileUri);
+
+
+    }
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+
+            // Get the dimensions of the View
+            int targetW = iv.getWidth();
+            int targetH = iv.getHeight();
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            File path = this.getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+            if (image == null){
+                image = getFile();
+            }
+            BitmapFactory.decodeFile(image.getAbsolutePath(), bmOptions);
 
 
 
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            Bitmap bitmap = null;
 
 
+            bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(), bmOptions);
+
+
+            iv.setImageBitmap(bitmap);
+
+        }
+
+    }
 }
